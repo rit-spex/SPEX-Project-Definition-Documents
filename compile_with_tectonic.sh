8e16cf27388bcfc706f9f9cbbeccfa9aa971f883
runphilrun <pjl7651@rit.edu>
2019-08-04 19:52:32 -0700

    shell script to compile with tectonic via docker

--- compile_with_tectonic.sh
+++ compile_with_tectonic.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Use this shell script to compile your TeX file using a Docker container
-# instead of your local machine. We use the docker image from 
+# instead of your local machine. We use the docker image from
 # https://hub.docker.com/r/dxjoke/tectonic-docker/
-
-# tectonic ➞ bibtex ➞ tectonic× 2
-docker run --mount src=$(1),target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c \
-"tectonic --keep-intermediates --reruns 0 $(2).tex; \
-bibtex $(2); \
-tectonic $(2).tex; tectonic $(2).tex"
+#
+# USAGE
+# ./compile_with_tectonic.sh /path/to/document/directory main.tex
+docker run -it --mount src="$1",target="/usr/src/tex",type=bind dxjoke/tectonic-docker tectonic $2
